a9c704e8b32909762a3ac06b5a40dde950f0a826
spongy <48900732+g3956@users.noreply.github.com>
2021-07-29 13:42:13 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=3 python tools/train_net.py --config-file configs/Market1501/AGW_R50.yml
---
+++ run_d.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=3 python tools/train_net.py --config-file configs/DukeMTMC/AGW_R50.yml
